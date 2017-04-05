26484fc0d770e1f1ab9902bc49241fe6179ae440
Emmanuelle Lejeail <manu.lejeail@gmail.com>
2017-04-05 09:07:11 +0200

    major changement in the structure
    reading of the config file in json
    new structure Config

--- integration.go
+++ integration.go
@@ -1,73 +1,78 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"io/ioutil"
-	"net/http"
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"github.com/TheThingsNetwork/ttn/core/types"
 	TTNmqtt "github.com/TheThingsNetwork/ttn/mqtt"
-
+	"net/http"
 )
 
-
-type ConfigOS struct {
-	apiURL	string
-	apiKey  string
-	devicePassword string 
-	deviceID string 
-	userName string 
-	topicName string 
+type Config struct {
+	OSapiURL         string `json: "OSapiURL"`
+	OSapiKey         string `json: "OSapiKey"`
+	OSdevicePassword string `json: "OSdevicePassword"`
+	OSdeviceID       string `json: "OSdeviceID"`
+	OSuserName       string `json: "OSuserName"`
+	OStopicName      string `json: "OStopicName"`
+	TTNapplicationID string `json: "TTNapplicationID"`
+	TTNdeviceID      string `json: "TTNdeviceID"`
+	TTNaccessKey     string `json: "TTNaccessKey"`
 }
 
-type ConfigTTN struct {
-	applicationID string
-	deviceID string
-	accessKey string 
-}
-
-//topic to get info from "office-app/devices/office-test/up"
-
-func main(){
-	var confOS ConfigOS
-	var confTTN ConfigTTN
-	//config on OpenSensors's side
-	confOS.apiKey="872946ba-f2e1-4e08-b5d0-80de02966023"
-	confOS.deviceID="6033"
-	confOS.devicePassword="zN9mr4Pn"
-	confOS.topicName="celcius"
-	confOS.userName="emmlej"
-	//config on TTN's side
-	confTTN.applicationID="office-app"
-	confTTN.accessKey="ttn-account-v2.OfuuW9smtu33PjpPtVAs54Bmc2dcgHEOywtuAT1oqzk"
-	confTTN.deviceID="office-hq"
-
+func main() {
+	/*var config Config
+	config.OSapiKey = "872946ba-f2e1-4e08-b5d0-80de02966023"
+	config.OSdeviceID = "6033"
+	config.OSdevicePassword = "zN9mr4Pn"
+	config.OStopicName = "celcius"
+	config.OSuserName = "emmlej"
+	config.TTNapplicationID = "office-app"
+	config.TTNaccessKey = "ttn-account-v2.OfuuW9smtu33PjpPtVAs54Bmc2dcgHEOywtuAT1oqzk"
+	config.TTNdeviceID = "office-hq"*/
+	
+	var config Config
+	file, err := ioutil.ReadFile("configuration.json")
+	if err != nil {
+		fmt.Sprintf("%s", err.Error())
+	}
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		fmt.Sprintf("%s", err.Error())
+	}
+	
 	//apiURL complete link
-	confOS.apiURL="https://realtime.opensensors.io/v1/topics//users/"+confOS.userName+"/"+confOS.topicName+"?client-id="+confOS.deviceID+"&password="+confOS.devicePassword
+	config.OSapiURL = "https://realtime.opensensors.io/v1/topics//users/" + config.OSuserName + "/" + config.OStopicName + "?client-id=" + config.OSdeviceID + "&password=" + config.OSdevicePassword
 
 	//connection to mqtt client of the things network
-	clientmqtt:=TTNmqtt.NewClient(nil, "emmlej", confTTN.applicationID, confTTN.accessKey, "tcp://eu.thethings.network:1883")
-	err:=clientmqtt.Connect()
-	if err!=nil{
+	clientmqtt := TTNmqtt.NewClient(nil, "emmlej", config.TTNapplicationID, config.TTNaccessKey, "tcp://eu.thethings.network:1883")
+	err = clientmqtt.Connect()
+	if err != nil {
 		fmt.Sprintf("error: connecting to the mqtt client %s", err.Error())
 	}
-	fmt.Sprintf("connected")
+	fmt.Printf("connected\n")
 
 	//Handler using the function to post the message to OpenSensors
 	handler := func(client TTNmqtt.Client, appID string, devID string, req types.UplinkMessage) {
 		fmt.Printf("\n*******MESSAGE INCOMING*******\n")
-		response, err := confOS.postMessage(req.PayloadFields); 
-		if err != nil || (response.StatusCode!=200 && response.StatusCode!=201 && response!=nil) {
-			fmt.Printf("Error while transmitting the message")
+		response, err := config.postMessage(req.PayloadFields)
+		if err != nil || (response.StatusCode != 200 && response.StatusCode != 201 && response != nil) {
+			if err != nil {
+				fmt.Sprintf("Error: %s", err.Error())
+			} else {
+				buffer := new(bytes.Buffer)
+				buffer.ReadFrom(response.Body)
+				fmt.Sprintf("Error : %s", buffer.String())
+			}
 		} else {
 			fmt.Printf("Your message was transmitted!")
 		}
 	}
 	//Subscribing to the device of TTN
-	token := clientmqtt.SubscribeDeviceUplink(confTTN.applicationID, confTTN.deviceID, handler)
-	fmt.Printf("...waiting for incoming messages...")
+	token := clientmqtt.SubscribeDeviceUplink(config.TTNapplicationID, config.TTNdeviceID, handler)
+	fmt.Printf("the subscription succeded\n...waiting for incoming messages...")
 	token.Wait()
 	if err := token.Error(); err != nil {
 		fmt.Sprintf("No subscription made %s", err.Error())
@@ -75,28 +80,29 @@ func main(){
 	//keeps the program running till a message arrives
 	select {}
 }
+
 //function used to post the message to OpenSensors
-func (confOS ConfigOS) postMessage(data map[string]interface{}) (*http.Response, error){
-	DataInString, err:=json.Marshal(data)
-	if err!=nil {
+func (config Config) postMessage(data map[string]interface{}) (*http.Response, error) {
+	DataInString, err := json.Marshal(data)
+	if err != nil {
 		return nil, err
 	}
 
-	DataTab:=make(map[string]interface{})
-	DataTab["data"]=string(DataInString[:])
-	message, err:=json.Marshal(DataTab)
-	if err!=nil{
+	DataTab := make(map[string]interface{})
+	DataTab["data"] = string(DataInString[:])
+	message, err := json.Marshal(DataTab)
+	if err != nil {
 		return nil, err
 	}
 
 	CliHTTP := &http.Client{}
 	DataInBytes := bytes.NewReader(message)
-	request, err := http.NewRequest("POST", confOS.apiURL, DataInBytes)
+	request, err := http.NewRequest("POST", config.OSapiURL, DataInBytes)
 	if err != nil {
 		return nil, err
 	}
 
 	request.Header.Add("Content-Type", "application/json")
-	request.Header.Add("Authorization", "api-key "+ confOS.apiKey)
+	request.Header.Add("Authorization", "api-key "+config.OSapiKey)
 	return CliHTTP.Do(request)
 }
